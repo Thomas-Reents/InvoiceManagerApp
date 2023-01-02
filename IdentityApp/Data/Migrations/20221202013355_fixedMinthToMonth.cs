33cac02846216ba8be8e14438e8ef09efd0044b0
Thomas R Reents <68446643+tomreents@users.noreply.github.com>
2023-01-02 17:12:11 -0600

    Add project files.

---
+++ IdentityApp/Data/Migrations/20221202013355_fixedMinthToMonth.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityApp.Data.Migrations
+{
+    public partial class fixedMinthToMonth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InvoiceMinth",
+                table: "Invoice",
+                newName: "InvoiceMonth");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InvoiceMonth",
+                table: "Invoice",
+                newName: "InvoiceMonth");
+        }
+    }
+}
